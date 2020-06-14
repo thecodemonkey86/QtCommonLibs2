d1a0ec6c09ea1dd0f225da51b00b66864f005d7e
stoeckerb <bernhard_stoecker@yahoo.de>
2020-06-14 23:15:06 +0200

    use QRandomGenerator instead of rand

--- util/string/stringutil.cpp
+++ util/string/stringutil.cpp
@@ -51,7 +51,7 @@ QString QtCommon2::StringUtil::randString( int length,const QString &chars)
     QString s(length, QChar('0'));
 
     for(int i=0;i<length;i++) {
-        s[i] = chars[rand()%chars.length()];
+        s[i] = chars[QRandomGenerator::system()->bounded(chars.length())];
     }
     return s;
 }
